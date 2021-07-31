82f0de2d27840c64622c552750812b0b5829ce5f
Alex1989-star <bio181@yandex.ru>
2021-07-31 11:24:27 +0300

    Initial Commit

---
+++ SwiftUIColorizedApp/SwiftUIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedAppApp.swift
+//  SwiftUIColorizedApp
+//
+//  Created by Леся on 31.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
